83a3ca7d90ef1167c38be624036eace8a91589de
“L-w-p-999” <1096152629@qq.com>
2025-05-11 21:02:21 +0800

    script c

--- sdkjs-plugins/content/test/scripts/helloWorld.js
+++ sdkjs-plugins/content/test/scripts/helloWorld.js
@@ -1,42 +1,59 @@
 // Example insert text into editors (different implementations)
-((window) => {
-    const text = "Hello world!"
-    window.Asc.plugin.init = () => {
-      const variant = 2
-  
-      switch (variant) {
+(function (window) {
+  const text = "Hello world!";
+
+  // 一定要用 function 而不是箭头函数，否则 this.callCommand 不可用
+  window.Asc.plugin.init = function () {
+    const variant = 2;
+
+    switch (variant) {
       case 0:
         // serialize command as text
-        let sScript = "var oDocument = Api.GetDocument();"
-        sScript += "oParagraph = Api.CreateParagraph();"
-        sScript += "oParagraph.AddText('Hello world!');"
-        sScript += "oDocument.InsertContent([oParagraph]);"
-        this.info.recalculate = true
-        this.executeCommand("close", sScript)
-        break
+        let sScript = "var oDocument = Api.GetDocument();";
+        sScript += "oParagraph = Api.CreateParagraph();";
+        sScript += "oParagraph.AddText('Hello world!');";
+        sScript += "oDocument.InsertContent([oParagraph]);";
+        this.info.recalculate = true;
+        this.executeCommand("close", sScript);
+        break;
+
       case 1:
         // call command without external variables
-        this.callCommand(() => {
-          const oDocument = Api.GetDocument()
-          const oParagraph = Api.CreateParagraph()
-          oParagraph.AddText("Hello world!")
-          oDocument.InsertContent([oParagraph])
-        }, true)
-        break
+        this.callCommand(function () {
+          const oDocument = Api.GetDocument();
+          const oParagraph = Api.CreateParagraph();
+          oParagraph.AddText("Hello world!");
+          oDocument.InsertContent([oParagraph]);
+        }, true);
+        break;
+
       case 2:
         // call command with external variables
-        Asc.scope.text = text // export variable to plugin scope
-        this.callCommand(() => {
-          const oDocument = Api.GetDocument()
-          const oParagraph = Api.CreateParagraph()
-          oParagraph.AddText(Asc.scope.text) // or oParagraph.AddText(scope.text);
-          oDocument.InsertContent([oParagraph])
-        }, true)
-        break
+        Asc.scope.text = text; // 导出到插件作用域
+        this.callCommand(function () {
+          const oDocument = Api.GetDocument();
+          const oParagraph = Api.CreateParagraph();
+          oParagraph.AddText(Asc.scope.text);
+          oDocument.InsertContent([oParagraph]);
+        }, true);
+        break;
+
       default:
-        break
-      }
+        break;
     }
-  
-    window.Asc.plugin.button = (id) => {}
-  })(window)
+  };
+
+  // 同样改成普通函数，以后按钮回调也能拿到 this
+  window.Asc.plugin.button = function (id) {
+    // 如果你想在按钮点击时才插入：
+    if (id === "btnInsertHello") {
+      Asc.scope.text = text;
+      this.callCommand(function () {
+        const oDocument = Api.GetDocument();
+        const oParagraph = Api.CreateParagraph();
+        oParagraph.AddText(Asc.scope.text);
+        oDocument.InsertContent([oParagraph]);
+      }, true);
+    }
+  };
+})(window);
